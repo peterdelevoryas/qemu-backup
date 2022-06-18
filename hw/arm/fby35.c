eccc8ae0644db3ecd025ca90c3f3266e3d549021
Peter Delevoryas <pdel@fb.com>
2022-06-17 17:03:47 -0700

    fby35: Boot BMC (AST2600)
    Use the following command to boot an image, using "-device loader" and
    "-drive" in conjunction:
    
        qemu-system-arm -machine fby35 -nographic \
            -device loader,file=fby35.mtd,addr=0,cpu-num=0 \
            -drive file=fby35.mtd,format=raw,if=mtd
    
    Signed-off-by: Peter Delevoryas <pdel@fb.com>

--- hw/arm/fby35.c
+++ hw/arm/fby35.c
@@ -21,19 +21,63 @@
  */
 
 #include "qemu/osdep.h"
+#include "qemu/units.h"
+#include "qapi/error.h"
 #include "hw/boards.h"
+#include "hw/qdev-properties.h"
+#include "hw/arm/aspeed_soc.h"
 
-#define FBY35_MACHINE_NR_CPUS (1)
+#define FBY35_BMC_NR_CPUS (2)
+#define FBY35_BMC_RAM_SIZE (2 * GiB)
+#define FBY35_BMC_HW_STRAP1 (0x000000C0)
+#define FBY35_BMC_HW_STRAP2 (0x00000003)
+
+#define FBY35_MACHINE_NR_CPUS (FBY35_BMC_NR_CPUS)
+#define FBY35_MACHINE_RAM_SIZE (FBY35_BMC_RAM_SIZE)
 
 #define TYPE_FBY35_MACHINE MACHINE_TYPE_NAME("fby35")
 OBJECT_DECLARE_SIMPLE_TYPE(Fby35MachineState, FBY35_MACHINE);
 
 struct Fby35MachineState {
     MachineState parent_obj;
+
+    MemoryRegion bmc_system_memory;
+    MemoryRegion bmc_dram;
+    MemoryRegion bmc_boot_rom;
+
+    AspeedSoCState bmc;
 };
+
+static void fby35_bmc_init(MachineState *machine)
+{
+    Fby35MachineState *s = FBY35_MACHINE(machine);
+    AspeedSoCClass *sc = NULL;
+
+    memory_region_init(&s->bmc_system_memory, OBJECT(s), "bmc-system-memory", UINT64_MAX);
+    memory_region_init(&s->bmc_dram, OBJECT(s), "bmc-dram", FBY35_BMC_RAM_SIZE);
+    memory_region_add_subregion(get_system_memory(), 0, &s->bmc_system_memory);
+    memory_region_add_subregion(&s->bmc_dram, 0, machine->ram);
+
+    object_initialize_child(OBJECT(s), "bmc", &s->bmc, "ast2600-a3");
+    object_property_set_int(OBJECT(&s->bmc), "ram-size", FBY35_BMC_RAM_SIZE, &error_abort);
+    object_property_set_link(OBJECT(&s->bmc), "system-memory", OBJECT(&s->bmc_system_memory), &error_abort);
+    object_property_set_link(OBJECT(&s->bmc), "dram", OBJECT(&s->bmc_dram), &error_abort);
+    qdev_prop_set_uint32(DEVICE(&s->bmc), "hw-strap1", FBY35_BMC_HW_STRAP1);
+    qdev_prop_set_uint32(DEVICE(&s->bmc), "hw-strap2", FBY35_BMC_HW_STRAP2);
+    qdev_prop_set_uint32(DEVICE(&s->bmc), "uart-default", ASPEED_DEV_UART5);
+    qdev_realize(DEVICE(&s->bmc), NULL, &error_abort);
+    sc = ASPEED_SOC_GET_CLASS(&s->bmc);
+
+    memory_region_add_subregion(&s->bmc_system_memory, sc->memmap[ASPEED_DEV_SDRAM], &s->bmc_dram);
+    memory_region_init_rom(&s->bmc_boot_rom, OBJECT(s), "bmc-boot-rom", 128 * MiB, &error_abort);
+    memory_region_add_subregion(&s->bmc_system_memory, 0, &s->bmc_boot_rom);
+
+    aspeed_board_init_flashes(&s->bmc.fmc, "n25q00", 2, 0);
+}
 
 static void fby35_machine_init(MachineState *machine)
 {
+    fby35_bmc_init(machine);
 }
 
 static void fby35_machine_class_init(ObjectClass *oc, void *data)
@@ -48,7 +92,7 @@ static void fby35_machine_class_init(ObjectClass *oc, void *data)
     mc->min_cpus = FBY35_MACHINE_NR_CPUS;
     mc->max_cpus = FBY35_MACHINE_NR_CPUS;
     mc->default_cpus = FBY35_MACHINE_NR_CPUS;
-    mc->default_ram_size = 0;
+    mc->default_ram_size = FBY35_MACHINE_RAM_SIZE;
 }
 
 static const TypeInfo fby35_types[] = {
