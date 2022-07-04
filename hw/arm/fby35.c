793cd342d566e6d13951676b15e1eba0eeb2049d
Peter Delevoryas <pdel@fb.com>
2022-07-04 19:27:50 +0200

    aspeed: Attempt to add AST1030 (BIC)
    Unfortunately, this results in an exception almost immediately:
    
        qemu: fatal: Lockup: can't escalate 3 to HardFault (current priority -1)
    
        R00=e9fffff8 R01=00000000 R02=00000000 R03=00000000
        R04=00000000 R05=00000000 R06=00000000 R07=00000000
        R08=00000000 R09=00000000 R10=00000000 R11=00000000
        R12=00000000 R13=e9fffff8 R14=fffffff9 R15=00010fe4
        XPSR=41000003 -Z-- T handler
        s00=00000000 s01=00000000 d00=0000000000000000
        s02=00000000 s03=00000000 d01=0000000000000000
        s04=00000000 s05=00000000 d02=0000000000000000
        s06=00000000 s07=00000000 d03=0000000000000000
        s08=00000000 s09=00000000 d04=0000000000000000
        s10=00000000 s11=00000000 d05=0000000000000000
        s12=00000000 s13=00000000 d06=0000000000000000
        s14=00000000 s15=00000000 d07=0000000000000000
        s16=00000000 s17=00000000 d08=0000000000000000
        s18=00000000 s19=00000000 d09=0000000000000000
        s20=00000000 s21=00000000 d10=0000000000000000
        s22=00000000 s23=00000000 d11=0000000000000000
        s24=00000000 s25=00000000 d12=0000000000000000
        s26=00000000 s27=00000000 d13=0000000000000000
        s28=00000000 s29=00000000 d14=0000000000000000
        s30=00000000 s31=00000000 d15=0000000000000000
        FPSCR: 00000000
    
    Not sure what this is, definitely going to ask the experts for help on this.
    
    To reproduce what I'm doing:
    
        wget https://github.com/facebook/openbmc/releases/download/openbmc-e2294ff5d31d/fby35.mtd
        wget https://github.com/peterdelevoryas/OpenBIC/releases/download/oby35-cl-2022.13.01/Y35BCL.elf
        qemu-system-arm -machine fby35 -drive file=fby35.mtd,format=raw,if=mtd \
            -device loader,file=fby35.mtd,addr=0,cpu-num=0 -nographic
    
    Signed-off-by: Peter Delevoryas <pdel@fb.com>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- hw/arm/fby35.c
+++ hw/arm/fby35.c
@@ -10,7 +10,9 @@
 #include "qapi/error.h"
 #include "sysemu/sysemu.h"
 #include "hw/boards.h"
+#include "hw/qdev-clock.h"
 #include "hw/arm/aspeed_soc.h"
+#include "hw/arm/boot.h"
 
 #define TYPE_FBY35 MACHINE_TYPE_NAME("fby35")
 OBJECT_DECLARE_SIMPLE_TYPE(Fby35State, FBY35);
@@ -21,11 +23,19 @@ struct Fby35State {
     MemoryRegion bmc_memory;
     MemoryRegion bmc_dram;
     MemoryRegion bmc_boot_rom;
+    MemoryRegion bic_memory;
+    Clock *bic_sysclk;
 
     AspeedSoCState bmc;
+    AspeedSoCState bic;
 };
 
 #define FBY35_BMC_RAM_SIZE (2 * GiB)
+#define FBY35_BMC_NR_CPUS 2
+
+#define FBY35_BIC_NR_CPUS 1
+
+#define FBY35_MACHINE_NR_CPUS (FBY35_BMC_NR_CPUS + FBY35_BIC_NR_CPUS)
 
 static void fby35_bmc_init(Fby35State *s)
 {
@@ -50,11 +60,31 @@ static void fby35_bmc_init(Fby35State *s)
     aspeed_board_init_flashes(&s->bmc.fmc, "n25q00", 2, 0);
 }
 
+static void fby35_bic_init(Fby35State *s)
+{
+    s->bic_sysclk = clock_new(OBJECT(s), "SYSCLK");
+    clock_set_hz(s->bic_sysclk, 200000000ULL);
+
+    memory_region_init(&s->bic_memory, OBJECT(s), "bic-memory", UINT64_MAX);
+
+    object_initialize_child(OBJECT(s), "bic", &s->bic, "ast1030-a1");
+    qdev_connect_clock_in(DEVICE(&s->bic), "sysclk", s->bic_sysclk);
+    object_property_set_link(OBJECT(&s->bic), "memory", OBJECT(&s->bic_memory),
+                             &error_abort);
+    aspeed_soc_uart_set_chr(&s->bic, ASPEED_DEV_UART5, serial_hd(1));
+    qdev_realize(DEVICE(&s->bic), NULL, &error_abort);
+
+    aspeed_board_init_flashes(&s->bic.fmc, "sst25vf032b", 2, 2);
+    aspeed_board_init_flashes(&s->bic.spi[0], "sst25vf032b", 2, 4);
+    aspeed_board_init_flashes(&s->bic.spi[1], "sst25vf032b", 2, 6);
+}
+
 static void fby35_init(MachineState *machine)
 {
     Fby35State *s = FBY35(machine);
 
     fby35_bmc_init(s);
+    fby35_bic_init(s);
 }
 
 static void fby35_class_init(ObjectClass *oc, void *data)
@@ -65,7 +95,7 @@ static void fby35_class_init(ObjectClass *oc, void *data)
     mc->init = fby35_init;
     mc->no_floppy = 1;
     mc->no_cdrom = 1;
-    mc->min_cpus = mc->max_cpus = mc->default_cpus = 2;
+    mc->min_cpus = mc->max_cpus = mc->default_cpus = FBY35_MACHINE_NR_CPUS;
 }
 
 static const TypeInfo fby35_types[] = {
