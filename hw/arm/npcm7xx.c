peterdelevoryas/qemu-backup

commit 73341fd328e36221c3e34b03fb0aadbb649c4d5b

hw/ssi: Make flash size a property in NPCM7XX FIU
This allows different FIUs to have different flash sizes, useful
in NPCM8XX which has multiple different sized FIU modules.

Signed-off-by: Hao Wu <wuhaotsh@google.com>
Reviewed-by: Patrick Venture <venture@google.com>

hw/arm/npcm7xx.c
@@ -274,17 +274,21 @@ static const struct {
     hwaddr regs_addr;
     int cs_count;
     const hwaddr *flash_addr;
+    size_t flash_size;
 } npcm7xx_fiu[] = {
     {
         .name = "fiu0",
         .regs_addr = 0xfb000000,
         .cs_count = ARRAY_SIZE(npcm7xx_fiu0_flash_addr),
         .flash_addr = npcm7xx_fiu0_flash_addr,
+        .flash_size = 128 * MiB,
+
     }, {
         .name = "fiu3",
         .regs_addr = 0xc0000000,
         .cs_count = ARRAY_SIZE(npcm7xx_fiu3_flash_addr),
         .flash_addr = npcm7xx_fiu3_flash_addr,
+        .flash_size = 128 * MiB,
     },
 };
 
@@ -686,6 +690,8 @@ static void npcm7xx_realize(DeviceState *dev, Error **errp)
 
         object_property_set_int(OBJECT(sbd), "cs-count",
                                 npcm7xx_fiu[i].cs_count, &error_abort);
+        object_property_set_int(OBJECT(sbd), "flash-size",
+                                npcm7xx_fiu[i].flash_size, &error_abort);
         sysbus_realize(sbd, &error_abort);
 
         sysbus_mmio_map(sbd, 0, npcm7xx_fiu[i].regs_addr);

include/hw/ssi/npcm7xx_fiu.h
@@ -60,6 +60,7 @@ struct NPCM7xxFIUState {
     int32_t cs_count;
     int32_t active_cs;
     qemu_irq *cs_lines;
+    size_t flash_size;
     NPCM7xxFIUFlash *flash;
 
     SSIBus *spi;

hw/ssi/npcm7xx_fiu.c
@@ -28,9 +28,6 @@
 
 #include "trace.h"
 
-/* Up to 128 MiB of flash may be accessed directly as memory. */
-#define NPCM7XX_FIU_FLASH_WINDOW_SIZE (128 * MiB)
-
 /* Each module has 4 KiB of register space. Only a fraction of it is used. */
 #define NPCM7XX_FIU_CTRL_REGS_SIZE (4 * KiB)
 
@@ -525,7 +522,7 @@ static void npcm7xx_fiu_realize(DeviceState *dev, Error **errp)
         flash->fiu = s;
         memory_region_init_io(&flash->direct_access, OBJECT(s),
                               &npcm7xx_fiu_flash_ops, &s->flash[i], "flash",
-                              NPCM7XX_FIU_FLASH_WINDOW_SIZE);
+                              s->flash_size);
         sysbus_init_mmio(sbd, &flash->direct_access);
     }
 }
@@ -543,6 +540,7 @@ static const VMStateDescription vmstate_npcm7xx_fiu = {
 
 static Property npcm7xx_fiu_properties[] = {
     DEFINE_PROP_INT32("cs-count", NPCM7xxFIUState, cs_count, 0),
+    DEFINE_PROP_SIZE("flash-size", NPCM7xxFIUState, flash_size, 0),
     DEFINE_PROP_END_OF_LIST(),
 };
 

commit 25dca366302bcc87be74c418080b0ad26b77d705

hw/ssi/nuvoton: Change flash_size to uint64_t
Previously this was declared as a size_t field that's used as a
DEFINE_PROP_SIZE property. The implementation of DEFINE_PROP_SIZE uses a
uint64_t and emits a compilation error based on the type mismatch:

    $ make
    changing dir to build for /Library/Developer/CommandLineTools/usr/bin/make ""...
      GIT     ui/keycodemapdb meson tests/fp/berkeley-testfloat-3 tests/fp/berkeley-softfloat-3 dtc slirp
    [1/18] Generating qemu-version.h with a custom command (wrapped by meson to capture output)
    [2/18] Compiling C object libcommon.fa.p/hw_ssi_npcm7xx_fiu.c.o
    FAILED: libcommon.fa.p/hw_ssi_npcm7xx_fiu.c.o
    cc -m64 -mcx16 -Ilibcommon.fa.p -I../dtc/libfdt -I../slirp -I../slirp/src -I/usr/local/Cellar/pixman/0.40.0/include/pixman-1 -I/Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk/usr/include -I/usr/local/Cellar/jpeg/9e/include -I/opt/homebrew/Cellar/zstd/1.4.3//include -I/usr/local/Cellar/libusb/1.0.26/include/libusb-1.0 -I/usr/local/Cellar/glib/2.72.2/include -I/usr/local/Cellar/glib/2.72.2/include/glib-2.0 -I/usr/local/Cellar/glib/2.72.2/lib/glib-2.0/include -I/usr/local/opt/gettext/include -I/usr/local/Cellar/pcre/8.45/include -fcolor-diagnostics -Wall -Winvalid-pch -std=gnu11 -O2 -g -iquote . -iquote /Users/pdel/qemu -iquote /Users/pdel/qemu/include -iquote /Users/pdel/qemu/tcg/i386 -DOS_OBJECT_USE_OBJC=0 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wstrict-prototypes -Wredundant-decls -Wundef -Wwrite-strings -Wmissing-prototypes -fno-strict-aliasing -fno-common -fwrapv -Wold-style-definition -Wtype-limits -Wformat-security -Wformat-y2k -Winit-self -Wignored-qualifiers -Wempty-body -Wnested-externs -Wendif-labels -Wexpansion-to-defined -Wno-initializer-overrides -Wno-missing-include-dirs -Wno-shift-negative-value -Wno-string-plus-int -Wno-typedef-redefinition -Wno-tautological-type-limit-compare -Wno-psabi -fstack-protector-strong -DSTRUCT_IOVEC_DEFINED -MD -MQ libcommon.fa.p/hw_ssi_npcm7xx_fiu.c.o -MF libcommon.fa.p/hw_ssi_npcm7xx_fiu.c.o.d -o libcommon.fa.p/hw_ssi_npcm7xx_fiu.c.o -c ../hw/ssi/npcm7xx_fiu.c
    ../hw/ssi/npcm7xx_fiu.c:543:5: error: 'uint64_t *' (aka 'unsigned long long *') and 'typeof (((NPCM7xxFIUState *)0)->flash_size) *' (aka 'unsigned long *') are not pointers to compatible types
        DEFINE_PROP_SIZE("flash-size", NPCM7xxFIUState, flash_size, 0),
        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/pdel/qemu/include/hw/qdev-properties.h:171:5: note: expanded from macro 'DEFINE_PROP_SIZE'
        DEFINE_PROP_UNSIGNED(_n, _s, _f, _d, qdev_prop_size, uint64_t)
        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/pdel/qemu/include/hw/qdev-properties.h:90:5: note: expanded from macro 'DEFINE_PROP_UNSIGNED'
        DEFINE_PROP(_name, _state, _field, _prop, _type,                       \
        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/pdel/qemu/include/hw/qdev-properties.h:71:15: note: expanded from macro 'DEFINE_PROP'
                + type_check(_type, typeof_field(_state, _field)),   \
                  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /Users/pdel/qemu/include/qemu/compiler.h:67:35: note: expanded from macro 'type_check'
                               ~~~~~~ ^ ~~~~~~
    1 error generated.

Fixes: 73341fd328e36 ("hw/ssi: Make flash size a property in NPCM7XX FIU")

hw/arm/npcm7xx.c
@@ -274,7 +274,7 @@ static const struct {
     hwaddr regs_addr;
     int cs_count;
     const hwaddr *flash_addr;
-    size_t flash_size;
+    uint64_t flash_size;
 } npcm7xx_fiu[] = {
     {
         .name = "fiu0",
@@ -690,8 +690,8 @@ static void npcm7xx_realize(DeviceState *dev, Error **errp)
 
         object_property_set_int(OBJECT(sbd), "cs-count",
                                 npcm7xx_fiu[i].cs_count, &error_abort);
-        object_property_set_int(OBJECT(sbd), "flash-size",
-                                npcm7xx_fiu[i].flash_size, &error_abort);
+        object_property_set_uint(OBJECT(sbd), "flash-size",
+                                 npcm7xx_fiu[i].flash_size, &error_abort);
         sysbus_realize(sbd, &error_abort);
 
         sysbus_mmio_map(sbd, 0, npcm7xx_fiu[i].regs_addr);

include/hw/ssi/npcm7xx_fiu.h
@@ -60,7 +60,7 @@ struct NPCM7xxFIUState {
     int32_t cs_count;
     int32_t active_cs;
     qemu_irq *cs_lines;
-    size_t flash_size;
+    uint64_t flash_size;
     NPCM7xxFIUFlash *flash;
 
     SSIBus *spi;